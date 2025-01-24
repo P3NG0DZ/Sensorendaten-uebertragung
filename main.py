d7d0ab9d775c313dbc9ded06f295d01afa72aace
Jannik Giera <jannikandgaming@gmail.com>
2025-01-24 09:37:36 +0000

    Programm Kommentiert für bessere Übersicht

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import sys
 from datetime import datetime
 
 def parse_sensor_data(data): ## Sensordaten parsen
-    start = data[0]
-    sensor_type = data[1]
-    sensor_number = data[2:4]
-    sign = data[4]
-    value = data[5:-1]
-    end = data[-1]
-    return start, sensor_type, sensor_number, sign, value, end
+    start = data[0] ## Startzeichen
+    sensor_type = data[1] ## Sensortyp
+    sensor_number = data[2:4] ## Sensornummer
+    sign = data[4] ## Vorzeichen
+    value = data[5:-1] ## Wert
+    end = data[-1] ## Endzeichen
+    return start, sensor_type, sensor_number, sign, value, end ## Rückgabe der Sensordaten
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
+if __name__ == "__main__": 
+    if len(sys.argv) != 2: ## Fehlermeldung, wenn Sensordaten fehlen bzw kein Übergabeparameter vorhanden ist
         print("Bitte starte das Programm folgendermaßen: python main.py <sensor_data>")
         sys.exit(1)
 
-    sensor_data = sys.argv[1]
+    sensor_data = sys.argv[1] ## Sensordaten
     print(f"Hallo:_ {sensor_data}")
 
     start, sensor_type, sensor_number, sign, value, end = parse_sensor_data(sensor_data) ## Sensordaten parsen
