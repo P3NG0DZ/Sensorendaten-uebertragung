612d6da8facee940e2ceafc2b83f34d2dd47078d
Jannik Giera <jannikandgaming@gmail.com>
2025-02-11 11:54:54 +0100

    Aktualisiere Skript zur Generierung von Sensordaten: füge LC_NUMERIC-Umgebungsvariable hinzu und ändere die Datenformatierung. Ergänze Semikolon am Ende der Sensordatenkette. Füge neue Messdaten in die CSV-Datei ein.

--- generate_sensor_data.sh
+++ generate_sensor_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LC_NUMERIC="C"
 
 # Funktion, um zufällige Sensordaten zu generieren
 generate_sensor_data() {
@@ -11,18 +12,21 @@ generate_sensor_data() {
     else
         sign="-"
     fi
-    local value=$(printf "%05.1f" $(echo "scale=1; $RANDOM/3276.7" | bc))
-    echo ":${sensor_type}${sensor_number}${sign}${value};"
+    local value=$(printf "%05.1f" $(awk -v min=0 -v max=1000 'BEGIN{srand(); print min+rand()*(max-min)}'))
+    echo "${sensor_type}${sensor_number}${sign}${value}"
 }
 
 # Benutzer nach der Anzahl der zu generierenden Daten fragen
 read -p "Wie viele Sensordaten möchten Sie generieren? " count
 
 # Sensordaten generieren und sammeln
-sensor_data=""
+sensor_data=":"
 for ((i = 0; i < count; i++)); do
     sensor_data+=$(generate_sensor_data)
 done
+
+# Semikolon am Ende der gesamten Sensordatenkette hinzufügen
+sensor_data+=";"
 
 # main.py mit den generierten Sensordaten als Übergabeparameter aufrufen
 python3 main.py "$sensor_data"
